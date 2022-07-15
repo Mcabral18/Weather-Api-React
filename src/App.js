4ac6c7b389449c227fe444789d721875f0f9cb25
LegendaryMarco21 <marcocabral@legendary.pt>
2022-07-15 15:39:32 +0100

    form fix fetch location

--- src/App.js
+++ src/App.js
@@ -1,72 +1,98 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import './App.css';
-import Input from './components/Input';
+import { useEffect, useState } from 'react';
+import { FiSearch } from 'react-icons/fi'
 
 function App() {
 
-  const [degrees, setDegrees] = useState(null)
+  //Set States for data
   const [location, setLocation] = useState("")
-  const [userLocation, setuserLocation] = useState("")
+  const [degrees, setDegrees] = useState(null)
   const [description, setDescription] = useState("")
   const [icon, setIcon] = useState("")
   const [humidity, setHumidity] = useState(null)
   const [wind, setWind] = useState(null)
   const [country, setCountry] = useState("")
-  const [dataFetched, setDataFetched] = useState(false)
+
+  //Set user Location
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
+
+  const getuserPosition = () => {
+    //Get user District
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        console.log(position);
+        const latitude = position.coords.latitude;
+        setLat(latitude);
+        const longitude = position.coords.longitude;
+        setLon(longitude)
+      },
+    );
+  }
+
+  const urlApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
 
 
-  const fetchData = async (e) => {
-    e.preventDefault()
 
+  const fetchDataWeatherDefault = async () => {
     try {
-      const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
-      const data = await res.data
+      const response = await fetch(urlApi);
+      const data = await response.json();
 
-      setDegrees(data.main.temp)
       setLocation(data.name)
+      setDegrees(data.main.temp)
       setDescription(data.weather[0].description)
       setIcon(data.weather[0].icon)
       setHumidity(data.main.humidity)
       setWind(data.wind.speed)
       setCountry(data.sys.country)
 
-      setDataFetched(true)
-    } catch (err) {
-      console.log(err)
-      alert("Please enter a valid location")
+    } catch (error) {
+      console.log("error", error);
     }
-
-  }
-
-  const defaultDataFetched = async () => {
-    if (!dataFetched) {
-      const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=accra&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
-      const data = await res.data
-
-      setDegrees(data.main.temp)
-      setLocation(data.name)
-      setDescription(data.weather[0].description)
-      setIcon(data.weather[0].icon)
-      setHumidity(data.main.humidity)
-      setWind(data.wind.speed)
-      setCountry(data.sys.country)
-    }
-
-  }
+  };
 
   useEffect(() => {
-    defaultDataFetched()
-  }, [])
+    fetchDataWeatherDefault();
+    getuserPosition();
+  }, []);
+
+  //Trigger fetch for location search
+
+  const getInputValue = async (e) => {
+    e.preventDefault()
+    //We can set the value to state setLocation(e.target.elements.localization.value)
+    let searchValue = e.target.elements.district.value;
+    try {
+
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
+      const data = await response.json();
+
+      setDegrees(data.main.temp)
+      setLocation(data.name)
+      setDescription(data.weather[0].description)
+      setIcon(data.weather[0].icon)
+      setHumidity(data.main.humidity)
+      setWind(data.wind.speed)
+      setCountry(data.sys.country)
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
 
   return (
     <div className="App">
       <div className='weather'>
-        <Input
-          text={(e) => setuserLocation(e.target.value)}
-          submit={fetchData}
-          func={fetchData}
-        />
+        <form onSubmit={getInputValue}>
+          <div className="input">
+            <input type={"text"} name="district" placeholder="Please enter location" className="input_value" />
+            <span className="input_icon">
+              <FiSearch />
+            </span>
+          </div>
+        </form>
 
         <div className='weather_display'>
           <h3 className='weather_location'>Weather in {location}</h3>
